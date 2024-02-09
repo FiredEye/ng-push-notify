ce1349b662c6187e4d3bf874148a6b97dc76d9e9
Rashik Tuladhar <rashiktuladhar13@gmail.com>
2024-02-09 17:30:00 +0545

    .

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NotifyService } from '../../services/notify.service';
 
 @Component({
   selector: 'app-about',
@@ -9,5 +10,8 @@ import { RouterLink } from '@angular/router';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  constructor(private notifyService:NotifyService){}
+  requestNotification(){
+    this.notifyService.requestnotifyPermission()
+    }
 }
